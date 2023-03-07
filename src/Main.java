995a6d83a165fb504c417251972d78494f7cfa02
Vitaliy Z <vv-z@mail.ru>
2023-03-07 16:11:47 +0300

    name editing

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         int balance = 200; // баланс телефона
-        int bonus_amount = 100; // полная сумма за которую дается 1 балл
+        int bonusAmount = 100; // полная сумма за которую дается 1 балл
         int refill = 1100; // сумма пополнения баланса
         if (refill > 1000) {
-            System.out.println("Вы пополнили баланс на " + refill + " руб., Вам начислено " + refill / bonus_amount + " бонусов. Ваш баланс с учетом бонусов составляет " + (refill + refill / bonus_amount + balance) + " руб.");
+            System.out.println("Вы пополнили баланс на " + refill + " руб., Вам начислено " + refill / bonusAmount + " бонусов. Ваш баланс с учетом бонусов составляет " + (refill + refill / bonusAmount + balance) + " руб.");
         } else {
 
             System.out.println("Ваш баланс = " + (refill + balance) + " руб.");
